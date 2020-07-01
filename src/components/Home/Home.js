a5e68b4c7efcff771e25c6348310b637ef33f0cf
Nick Ramsey <nramseysc@gmail.com>
2020-07-01 01:25:11 -0700

    vehicle browser stub

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css';
 import SiteCarousel from '../SiteCarousel';
+import VehicleBrowser from '../VehicleBrowser';
+
 
 class Home extends React.Component{
     
@@ -12,6 +14,7 @@ class Home extends React.Component{
         return(
             <div>
                 <SiteCarousel />
+                <VehicleBrowser />
             </div>
         );
     }
