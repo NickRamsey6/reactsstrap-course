98e848d2b73f8215765191d3b10723a95b4d30d9
Nick Ramsey <nramseysc@gmail.com>
2020-07-03 01:27:58 -0700

    vehicle browser setup

--- src/components/VehicleBrowser/VehicleBrowser.js
+++ src/components/VehicleBrowser/VehicleBrowser.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './VehicleBrowser.css';
+import { Container, Row, Col, Card, CardImg, CardText, CardBody, CardTitle,CardSubtitle, NavLink } from 'reactstrap';
+
 
 class VehicleBrowser extends React.Component{
     constructor(props){
@@ -7,7 +9,27 @@ class VehicleBrowser extends React.Component{
     }
 
     render(){
-        return <h2>Vehicle Browser</h2>;
+        return(
+            <div>
+                <Container>
+                    <Row>
+                        <Col md={4}>
+                            <Card>
+                                <CardImg top width="100%" src={"/replaceme.png"} alt="replace me" />
+                                <CardBody>
+                                    <CardTitle>Model Year Model Name</CardTitle>
+                                    <CardSubtitle>{"tagline here"}</CardSubtitle>
+                                    <CardText>Start at {"$1"}</CardText>
+                                    <NavLink>Details</NavLink>
+                                    <NavLink>Build and Price</NavLink>
+                                    <NavLink>Dealers Near You</NavLink>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
     }
 }
 
